feab728d2cf39cf121dfff9bb0b5fd2dfa1afd34
RainMark <rain.by.zhou@gmail.com>
2022-07-05 23:46:35 +0800

    remove ssl v2/v3
    Signed-off-by: RainMark <rain.by.zhou@gmail.com>

--- nmp/connection.py
+++ nmp/connection.py
@@ -38,8 +38,6 @@ class ConnectionPool:
     def new_ssl_context():
         context = ssl.create_default_context()
         context.options |= ssl.OP_NO_COMPRESSION
-        context.options |= ssl.OP_NO_SSLv2
-        context.options |= ssl.OP_NO_SSLv3
         context.options |= ssl.OP_NO_TLSv1
         context.options |= ssl.OP_NO_TLSv1_1
         if ssl.HAS_TLSv1_3:
