bc05e1462b3006f2f0978e24301e99c29ab67c73
Rain Mark <rain.by.zhou@gmail.com>
2026-09-20 14:11:28 +0800

    prepare v0.1 desktop release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nmp',
-    version='0.0.1',
+    version='0.1.0',
     author='RainMark',
     author_email='rain.by.zhou@gmail.com',
     description='Network Multistage Pxxxx/Net Manager Project',
